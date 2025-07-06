751ce36ea05cf248c6b246726730431c0defb0cd
nibirnafis <nibirnafis@gmail.com>
2025-07-06 21:24:53 +0600

    image link fixed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
         <div>
             <div className="flex justify-center items-center relative">
                 <p className="absolute text-white text-5xl md:text-7xl font-bold pb-4 text-center">Welcome To<br />Online Book Shop</p>
-                <img className="mb-10 rounded-2xl " src="/src/assets/library.jpg" alt="" />
+                <img className="mb-10 rounded-2xl " src="https://i.ibb.co/5hn4bMfR/library.jpg" alt="" />
                 
             </div>
         </div>
